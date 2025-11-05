6f957edf127fc073ae438b43ae2f1cc5ce322846
Ojass Bhatt <ojassbhatt@gmail.com>
2025-11-05 04:50:11 +0000

    Error: A tree hydrated but some attributes of the server rendered HTML d

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({
           inter.variable,
           poppins.variable
         )}
+        suppressHydrationWarning
       >
         <ThemeProvider
           attribute="class"
